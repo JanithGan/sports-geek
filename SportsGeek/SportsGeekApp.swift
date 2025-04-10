15c6e710b8c4631168ceebfd1a94b87c9bb56016
Janith Ganepola <janithgan@gmail.com>
2025-04-10 23:51:20 +0530

    Add inital boilerplates

---
+++ SportsGeek/SportsGeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsGeekApp.swift
+//  SportsGeek
+//
+//  Created by Janith Ganepola on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct SportsGeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportsGeekTests/SportsGeekTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportsGeekTests.swift
+//  SportsGeekTests
+//
+//  Created by Janith Ganepola on 2025-04-10.
+//
+
+import Testing
+@testable import SportsGeek
+
+struct SportsGeekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
