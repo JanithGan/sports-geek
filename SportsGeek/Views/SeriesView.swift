cbb8306ba49c8641f2de47a55fb44b1e73cf7e93
PramudithaKaushalya <pramuditha.kaushalya@gmail.com>
2025-05-07 12:17:33 +0530

    Fixing issues

--- SportsGeek/Views/SeriesView.swift
+++ SportsGeek/Views/SeriesView.swift
@@ -11,9 +11,9 @@ struct SeriesView: View {
     let tournament: Tournament
 
     let mockSeriesList: [Series] = [
-        Series(id: "1", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2025-02-21", endDate: "2025-02-28", year: "2025", host: "Sri Lanka", wonBy: "Sri Lanka"),
-        Series(id: "2", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2024-03-13", endDate: "2024-03-18", year: "2024", host: "Bangladesh", wonBy: "Bangladesh"),
-        Series(id: "3", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2023-01-05", endDate: "2023-01-10", year: "2023", host: "Sri Lanka", wonBy: "Sri Lanka")
+        Series(id: "1", tournamentId: "1", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2025-02-21", endDate: "2025-02-28", year: "2025", host: "Sri Lanka", wonBy: "Sri Lanka"),
+        Series(id: "2", tournamentId: "1", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2024-03-13", endDate: "2024-03-18", year: "2024", host: "Bangladesh", wonBy: "Bangladesh"),
+        Series(id: "3", tournamentId: "1", name: "ODI Series Bangladesh vs. Sri Lanka", startDate: "2023-01-05", endDate: "2023-01-10", year: "2023", host: "Sri Lanka", wonBy: "Sri Lanka")
     ]
 
     var filteredSeries: [Series] {
